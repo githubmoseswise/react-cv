019a3f5b20a8686dcb60c8f00ac023eecbcf3893
moseswisegit <moseswisegit@gmail.com>
2024-07-24 22:30:14 +0000

    add image not work

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,65 +1,99 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project1.JPG'
+    name: "F&MMCI",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://fmmci.net/",
+    info: "FMMCI est une société de Consulting qui fournit des produits et équipements de technologies, d’ingénierie et scientifiques aux clients du gouvernement et dans divers secteurs tels que Télécoms, Energies, Industrie manufacturière, sécurités nationales, Banque et Finance, Santé, Automobile, Assurance, Aéronautique et défense, Hôtellerie et voyage, Infrastructure et Transport, Environnement, Education, F&MMCI Motors etc.",
+    picture: "./media/project1.JPG",
   },
   {
     id: 2,
-    name: 'lorem.fr',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.JPG'
+    name: "SUPERMAPCI",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://supermapci.com/",
+    info: "SuperMapCI est le distributeur exclusif des suites de logiciels SIG SuperMap en Côte d’Ivoire. En tant que représentant commercial, elle mènera également des activités de distribution dans les autres pays de la CEDEAO. Nous proposons une gamme complète de produits, incluant : SuperMap iDesktopX – V11, SuperMap iServer – V11, SuperMap iObjects (versions .NET, Java, C++) V11, SuperMap iMobile (versions Android, iOS) V11, SuperMap iPortal V11, et bien d’autres.",
+    picture: "./media/project2.JPG",
   },
   {
     id: 3,
-    name: 'dolorset.fr',
-    languages: ['javascript','php','css'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    name: "SUPERGEOAFRICA",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://supergeoafrica.com/",
+    info: "Fondé en 2016 la société SUPERGEO AFRICA est fournisseur de solutions technologiques et de services en géomatique.Avec une équipe d’experts dotés de plus de 25 ans d’expérience.",
+    picture: "./media/project3.JPG",
   },
+
   {
     id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','php','react'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project4.JPG'
+    name: "JUNOOPAY",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://junoopay.junoox.com/",
+    info: "JUNOOPAY est une plateforme innovante de transfert d’argent et de gestion financière, offrant une gamme complète de services pour répondre aux besoins de nos utilisateurs. Depuis notre création, nous nous engageons à simplifier les transactions financières et à offrir des solutions pratiques pour les particuliers, les entreprises et les institutions.",
+    picture: "./media/project4.JPG",
   },
+
   {
     id: 5,
-    name: 'repudiante.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project5.JPG'
+    name: "JUNOOMAP",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://junoomap.net/",
+    info: "JUNOOMAP est une plateforme de cartographie offre une variété de fonctionnalités pour permettre aux utilisateurs d'explorer, d'analyser et de tirer parti des données géospatiales.",
+    picture: "./media/project5.JPG",
   },
+
   {
     id: 6,
-    name: 'test.io',
-    languages: ['javascript','react', 'css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project6.JPG'
+    name: "JUNOOTRAFIC",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://junootraffic.net/",
+    info: "Nous vous permettons d’utiliser nos données pour votre solution de gestion de trafic, de gestion de plateforme automobile, de gestion de sécurité, etc.",
+    picture: "./media/project6.JPG",
   },
+
   {
     id: 7,
-    name: 'dolorset.fr',
-    languages: ['javascript','php','css'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    name: "JUNOOVTC",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://taxi.junoox.com/",
+    info: "JUNOOVTC est un service en ligne qui permet de commander des taxis en toute sécurité pour vos déplacements à des prix négociés. La plateforme vous permet de vous connecter à tous les types de taxi selon le standing demandé en temps réel.",
+    picture: "./media/project3.JPG",
   },
-]
+
+  {
+    id: 8,
+    name: "JUNOODELI",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://deli.junoox.com/",
+    info: "La plateforme JUNOODELI vous permet de livrer vos marchandises à destination, de commander les plats ou les produits et les réceptionner en toute sécurité. Si vous êtes une boutique, fastfood, un magasin ou un supermarché qui livre les produits, JUNOODELI vous permet de suivre et superviser votre livraison en toute sécurité.",
+    picture: "./media/project3.JPG",
+  },
+
+  // {
+  //   id: 9,
+  //   name: "JUNOOX",
+  //   languages: ["javascript", "html", "css"],
+  //   languagesIcons: ["fab fa-js", "fab fa-html", "fab fa-css3-alt"],
+  //   source: "https://junoox.com/",
+  //   info: "Moteur de recherche gratuit et libre d'accès en Afrique et dans le monde",
+  //   picture: "./media/project3.JPG",
+  // },
+
+  {
+    id: 9,
+    name: "JUNOOX",
+    languages: ["wordpress", "javascript", "css"],
+    languagesIcons: ["fab fa-wordpress"],
+    source: "https://junoox.com/",
+    info: "Moteur de recherche gratuit et libre d'accès en Afrique et dans le monde",
+    picture: "./media/project3.JPG",
+  },
+];
