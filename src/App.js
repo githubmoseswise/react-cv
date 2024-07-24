9278d384be0d81228853f3df30b169e9f0312d69
moseswisegit <moseswisegit@gmail.com>
2024-07-24 23:15:07 +0000

    correct refresh page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,useRoutes} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Knownledges from './pages/Knownledges';
 import Contact from './pages/Contact';
@@ -7,23 +7,16 @@ import Portfolio from './pages/Portfolio';
 import NotFound from './pages/NotFound';
 
 const App = () => {
- 
-  const MyRoutes = () => {
-    let routes = useRoutes([
-      { path: "/", element: <Home /> },
-      { path: "/competences", element: <Knownledges /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/portfolio", element: <Portfolio /> },
-      { path: "/*", element: <NotFound /> },
-
-    ]);
-    return routes;
-  };
-
   return (
-      <BrowserRouter>
-            <MyRoutes/>
-      </BrowserRouter>       
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/competences" element={<Knownledges />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
