b87b541cb7c6287b570db594bf4f8b14acc10e0d
moseswisegit <moseswisegit@gmail.com>
2024-07-25 09:27:52 +0000

    retouche taille radioDisplay

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -34,7 +34,7 @@ class Project extends Component {
                                     <h2>{name}</h2>
                                     <div className="sourceCode">
                                         <a href={source} rel="noopener noreferrer" 
-                                        className="button" target="_blank"> Code source ou Url</a>
+                                        className="button" target="_blank"> Code source</a>
                                     </div>
                                 </div>
                                     <p className="text">{info}</p>
