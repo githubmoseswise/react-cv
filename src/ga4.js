ea89d41344eaf4b58df45cd92019f810f9a20e45
moseswisegit <moseswisegit@gmail.com>
2024-07-25 08:39:10 +0000

    ajout de google analytique

--- src/ga4.js
+++ src/ga4.js
@@ -2,7 +2,7 @@
 import ReactGA from 'react-ga4';
 
 const initializeGA = () => {
-  ReactGA.initialize('G-86BXWY91N7'); // Remplacez par votre ID de mesure Google Analytics
+  ReactGA.initialize('G-N9J8E48X5S'); // Remplacez par votre ID de mesure Google Analytics
 };
 
 export default initializeGA;
