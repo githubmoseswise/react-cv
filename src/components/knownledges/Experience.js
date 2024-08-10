dd41351c738fa8710dba9636ac9e4dd759fe91e8
moseswisegit <moseswisegit@gmail.com>
2024-08-10 10:05:20 +0000

    update information

--- src/components/knownledges/Experience.js
+++ src/components/knownledges/Experience.js
@@ -5,72 +5,73 @@ const Experience = () => {
     <div className="experience">
       <h3> Experiences</h3>
       <div className="exp-1">
-        <h4> SuperMap - Lead Développeur SIG | F&MMCI</h4>
-        <h5>Jan. 2024 - présent (Cocody, Côte d'Ivoire)</h5>
+        <h4> Développeur Full stack & Mobile | Edn computing group</h4>
+        <h5> 04/2021 - Présent - présent (AKPAKPA, Bénin)</h5>
         <p>
           <ul className="square-bullets">
             <li>
-              Participé au développement d'application SIG de Cacao avec
-              SuperMap Iportal et Iclient.
+              Concevoir des applications web en utilisant la méthode Agile,
+              comme une application de gestion des salaires et des employés avec
+              Laravel et React.js.
             </li>
             <li>
-              Hébergé des cartes géospatiales sur Iserver, facilitant l'accès et
-              l'interaction avec les données cartographiques pour les
-              utilisateurs finaux.
+              Créer et gérer des bases de données avec MySQL tout en développant
+              des services backend efficaces pour une intégration fluide des
+              fonctionnalités.
             </li>
             <li>
-              Collaboré avec des équipes pour comprendre les besoins des
-              utilisateurs et fournir un support technique de base, tout en
-              créant des visualisations cartographiques simples et efficaces.
+              Développer et intégrer des services et des API RESTful pour
+              garantir une communication fluide entre les applications et les
+              serveurs, en utilisant Insomnia et Postman pour les tests.
+            </li>
+
+            <li>
+              Gérer la configuration et la maintenance des serveurs pour
+              garantir le bon fonctionnement des applications.
+            </li>
+
+            <li>
+              Rédiger la documentation des applications développées pour assurer
+              une utilisation et une maintenance efficaces
             </li>
           </ul>
-        </p>
-      </div>
-      <div className="exp-2">
-        <h4>Développeur web C# | CNTIG</h4>
-        <h5>Fév. 2023 - Nov. 2023 (Cocody, Côte d'Ivoire)</h5>
-        <p>
-        <ul className="square-bullets">
-            <li>
-              Détection et résolution rapide des anomalies,garantissant une expérience utilisateur sans faille.
-            </li>
-            <li>
-              Pilotagecde l'identification des exigences d'intégration de l'API SuperGéo afin d'améliorer l'affichage de cartes interactives dans certaines applications.
-            </li>
-            <li>
-             Mise en place d'un système d'importation des données dans SQL SERVER.
-            </li>
-          </ul>
-        </p>
-      </div>
-      <div className="exp-3">
-        <h4>Analyste programmeur PHP/Laravel (Télétravail)</h4>
-        <h5>Mars. 2022 - présent (France)</h5>
-        <p>
-            <ul className="square-bullets">
-                <li>
-                 Participation active à la conception d'une application dédiée aux commerciaux (CRM) en cours de développement.
-                </li>
-                <li>
-                    Actuellement impliqué dans la création d'une application de sondage destinée au élections en France.
-                </li>
-            </ul>
         </p>
       </div>
 
-      <div className="exp-4">
+      <div className="exp-2">
+        <h4>Analyste programmeur PHP/Laravel (Télétravail)</h4>
+        <h5>Mars. 2022 - présent (France)</h5>
+        <p>
+          <ul className="square-bullets">
+            <li>
+              Collaborer avec des développeurs à la conception et à la
+              réalisation de l'application CRM "Chef des Ventes" (CDV) dédiée
+              aux commerciaux, en utilisant Laravel.
+            </li>
+            <li>
+              Actuellement impliqué dans la création d'une application de
+              sondage destinée au élections en France.
+            </li>
+          </ul>
+        </p>
+      </div>
+
+      <div className="exp-3">
         <h4>Analyste programmeur | MYBEATUS</h4>
         <h5>Octobre. 2020 - Avr. 2021 (Bénin)</h5>
         <p>
-            <ul className="square-bullets">
-                <li>
-                  Analyse approfondie des codes existants avec propositions de nouvelles lignes de codes développées en 
-                  utilisant les technologies Laravel et Angular
-                </li>
-                <li>
-                    Conception et dispensation de formations destinées aux enseignants et aux parents d'élèves pour une utilisation optimale de la plateforme mybeatus.com
-                </li>
-            </ul>
+          <ul className="square-bullets">
+            <li>
+              Analyse approfondie des codes existants avec propositions de
+              nouvelles lignes de codes développées en utilisant les
+              technologies Laravel et Angular
+            </li>
+            <li>
+              Conception et dispensation de formations destinées aux enseignants
+              et aux parents d'élèves pour une utilisation optimale de la
+              plateforme mybeatus.com
+            </li>
+          </ul>
         </p>
       </div>
 
@@ -78,11 +79,12 @@ const Experience = () => {
         <h4>Analyste programmeur | Poste du Bénin</h4>
         <h5>Avr. 2020 - Octobre. 2020 (Bénin)</h5>
         <p>
-            <ul className="square-bullets">
-                <li>
-                  Mise en place d'une application de courier retour et de gestion de stock pour le compte de mon mémoire
-                </li>
-            </ul>
+          <ul className="square-bullets">
+            <li>
+              Mise en place d'une application de courier retour et de gestion de
+              stock pour le compte de mon mémoire
+            </li>
+          </ul>
         </p>
       </div>
     </div>
